f4aabde6fcf9b0055f62796afa3f8e14cf096c64
santhoshkumaryadav111 <162984327+santhoshkumaryadav111@users.noreply.github.com>
2024-03-13 09:27:21 +0530

    Add files via upload

---
+++ local.py
@@ -0,0 +1,28 @@
+'''
+print("Hello World")
+#print("Buy")
+'''
+print("Santhosh")
+
+
+'''
+1.Commenting is used for making documentation of a python file.
+2.when the line is commented,that line is not considered as a part of the code.
+3.when the program is excuted ,commented line will not be in the part of execution.
+
+
+
+------->NOTE:
+    single line commenting can be done using # at the beginning of the line
+    Eg:
+        print("Hello WORLD")
+Multiple line commenting can be done using """ code """.capitalize.the lines between '''
+
+#print("HELLO WORLD")
+
+import this
+print(this)
+import ios a keyword which is used to communicate between the python files.
+import antigravity
+a=2
+
