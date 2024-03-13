1880bc9db6c0d59b128cbd656c5da481af9d5963
santhoshkumaryadav111 <162984327+santhoshkumaryadav111@users.noreply.github.com>
2024-03-13 09:30:24 +0530

    Add files via upload

---
+++ santhosh.py
@@ -0,0 +1,64 @@
+
+
+# <<, >>
+# print(5>>1)
+# 16>4
+
+# ! Logical
+#and, or , not
+a=6
+#print(a>3 and a<10)
+#print(a>7 or a<30)
+#print(not(a>8 and a<10))
+
+#! Identify operator
+# is, is not
+a=8
+b=8
+#print(a is b)
+#print(a==b)
+
+A=(1, 2, 3)
+B=(1, 2, 3)
+print(a is not b)
+
+# ! Membership operator
+# in, not in
+#11=[7,8, 9, 0, 6, 5]
+#num=55
+#print(num not in l1)
+
+#num=678
+#print(8 in num) # error
+
+# ! conditional statement
+if, elif, else
+
+#Eg:1
+# --> C syntax
+# if (condition){
+#     statement;
+#     statement;
+#     statement;
+#     statement;
+# statement
+
+# eg:1
+# a=6
+# if a:
+#    print("hello")
+# Eg:2
+# a=6
+ # else
+
+ # a number is even or odd
+  int a=2
+  c=a/2
+  print("c is odd")
+
+
+
+
+
+
+
